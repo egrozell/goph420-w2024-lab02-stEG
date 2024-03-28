069b6e5d9961c5e1a6da37ead1bef16bd0bc729b
egrozell <ethan.grozell@ucalgary.ca>
2024-03-28 01:06:24 -0600

    nothing new just testing my ide

--- src/goph420_lab02/root_finder.py
+++ src/goph420_lab02/root_finder.py
@@ -1,41 +1,47 @@
-import numpy as np 
+import numpy as np
+
+
 def root_newton_raphson(x0, f, dfdx):
     """
     inputs
     ______
-    x0: float the initial guess of root 
+    x0: float the initial guess of root
     f: callable function
     dfdx: callable function which is the first derivative of f
     outputs
     _______
     double: float
-    integer: int 
+    integer: int
     x_array: numpy.ndarray
 
     raises
-    ______ 
-    ValueError: if x0 is not float like 
+    ______
+    ValueError: if x0 is not float like
     TypeError: if f is not callable
     TypeError: if dfdx is not callable
     """
+
     pass
-def root_secant_modified(x0, dx ,f):
+
+
+def root_secant_modified(x0, dx, f):
     """
     inputs
     ______
-    x0: float the initial guess of root 
+    x0: float the initial guess of root
     dx: is a step size for derivative estimation
     f: callable function
     outputs
     _______
     double: float
-    integer: int 
+    integer: int
     x_array: numpy.ndarray
 
     raises
-    ______ 
-    ValueError: if x0 is not float like 
-    ValueError: if dx is not float like 
+    ______
+    ValueError: if x0 is not float like
+    ValueError: if dx is not float like
     TypeError: if f is not callable
     """
+
     pass
